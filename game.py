e2431aa35572f364f09f217d5bda20f855c5f3bf
Oxerene <dipankerv2@gmail.com>
2024-10-13 00:45:37 +0530

    Modified

--- game.py
+++ game.py
@@ -1,5 +1,5 @@
-import pygame, sys
-from scripts.entities import PhysicsEntity, Player
+import pygame, sys, random, os
+from scripts.entities import PhysicsEntity, Player, Enemy
 from scripts.utils import load_image, load_images, Animation
 from scripts.tilemap import Tilemap
 from scripts.clouds import Clouds
@@ -18,29 +18,69 @@ class Game:
         self.movement = [False, False] #by default its idle, no movement on x-axis and y-axis
         
         self.assets = {
-            'grass' : load_images('platform/grass'),
-            'stone' : load_images('platform/stone'),
+            'decor': load_images('tiles/decor'),
+            'grass': load_images('tiles/grass'),
+            'large_decor': load_images('tiles/large_decor'),
+            'stone': load_images('tiles/stone'),
             'background' : load_image('background/bg.png'),
             'clouds' : load_images('clouds'),
             'player/idle' : Animation(load_images('entities/player/idle'), img_dur=10),
-            'player/run' : Animation(load_images('entities/player/run'), img_dur=6),
-            'player/roll' : Animation(load_images('entities/player/roll')),
-            'player/jump' : Animation(load_images('entities/player/jump'))
+            'player/run' : Animation(load_images('entities/player/run'), img_dur=8),
+            'player/roll' : Animation(load_images('entities/player/roll'), loop=False),
+            'player/jump' : Animation(load_images('entities/player/jump')),
+            'player/attack' : Animation(load_images('entities/player/attack'), img_dur= 5),
+            'enemy/idle' : Animation(load_images('entities/enemy/idle'), img_dur=6),
+            'enemy/run' : Animation(load_images('entities/enemy/run'), img_dur=10),
+            'enemy/attack' : Animation(load_images('entities/enemy/attack'), img_dur = 7),
+            'enemy/death' : load_image('entities/enemy/death/03.png')
         }
-        
-        
+            
         self.clouds = Clouds(self.assets['clouds'])
         
-        self.player = Player(self, (50, 50), (13, 19))
-        
         self.tilemap = Tilemap(self, tile_size=16)
+        self.level = 0
+        self.load_level(self.level)
+        
+        self.screenshake = 0
+        
+    def load_level(self, map_id):
+        self.tilemap.load('data/maps/' + str(map_id) + '.json')
+        
+        self.player = Player(self, (50, 50), (15, 19))
+        
+        self.enemies = []
+        for spawner in self.tilemap.extract([('spawners', 0), ('spawners', 1)]):
+            if spawner['variant'] == 0:
+                self.player.pos = spawner['pos']
+            else:
+                self.enemies.append(Enemy(self, spawner['pos'], (22, 15)))
 
         self.scroll = [0, 0]
+        self.dead = 0
+        self.transition = -30
 
     def run(self):
         #game loop
         while True:
-            self.display.blit(self.assets['background'], (0, 0))                   #refreshes the screen to default display as well as remove trail issue with movement
+            self.display.blit(self.assets['background'], (0, 0))       #refreshes the screen to default display as well as remove trail issue with movement
+            
+            self.screenshake = max(0, self.screenshake - 1)
+            
+            if not len(self.enemies):
+                self.transition += 1
+                if self.transition > 30:
+                    self.level = min(self.level + 1, len(os.listdir('data/maps')) - 1)
+                    self.load_level(self.level)
+            if self.transition < 0:
+                self.transition += 1
+            
+            if self.dead:
+                self.dead += 1
+                if self.dead >= 10:
+                    self.transition = min(30, self.transition + 1)
+                if self.dead > 60:
+                    self.load_level(self.level)
+            
             
             self.scroll[0] += (self.player.rect().centerx - self.display.get_width() / 2 - self.scroll[0]) / 30
             self.scroll[1] += (self.player.rect().centery - self.display.get_height() / 2 - self.scroll[1]) / 30
@@ -50,14 +90,34 @@ class Game:
             self.clouds.render(self.display, offset=render_scroll)
             
             self.tilemap.render(self.display, offset = render_scroll)
-            self.player.update(self.tilemap, (self.movement[1] - self.movement[0], 0))
-            self.player.render(self.display, offset = render_scroll)
+
+            for enemy in self.enemies.copy():
+                kill = enemy.update(self.tilemap, (0, 0))
+                enemy.render(self.display, offset=render_scroll)
+                if kill:
+                    self.enemies.remove(enemy)
+                    self.dead = 0
+
+            if self.dead <= 60:
+                self.player.update(self.tilemap, (self.movement[1] - self.movement[0], 0))
+                self.player.render(self.display, offset = render_scroll)
+                if self.player.killed:
+                    self.dead += 1
+                    self.screenshake = max(16, self.screenshake)
+
             
             for event in pygame.event.get(): #interact with the windows and get inputs
                 #checks to see if we pressed X(cross) on windows
                 if event.type == pygame.QUIT: 
                     pygame.quit()
                     sys.exit()
+                
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    if event.button == 1:
+                        self.player.attack()
+                if event.type == pygame.MOUSEBUTTONUP:
+                    if event.button == 1:
+                        self.player.attack()
                 
                 #check movement by event listening to Key presses
                 if event.type == pygame.KEYDOWN:
@@ -66,7 +126,7 @@ class Game:
                     if event.key == pygame.K_RIGHT or event.key == pygame.K_d:
                         self.movement[1] = True
                     if event.key == pygame.K_UP or event.key == pygame.K_SPACE:
-                        self.player.velocity[1] = -3
+                        self.player.jump()
                     
                 if event.type == pygame.KEYUP:
                     if event.key == pygame.K_LEFT or event.key == pygame.K_a:   
@@ -74,7 +134,14 @@ class Game:
                     if event.key == pygame.K_RIGHT or event.key == pygame.K_d:
                         self.movement[1] = False
             
-            self.screen.blit(pygame.transform.scale(self.display, self.screen.get_size()), (0, 0))
+            if self.transition:
+                transition_surf = pygame.Surface(self.display.get_size())
+                pygame.draw.circle(transition_surf, (255, 255, 255), (self.display.get_width() // 2, self.display.get_height() // 2), (30 - abs(self.transition)) * 8)
+                transition_surf.set_colorkey((255, 255, 255))
+                self.display.blit(transition_surf, (0, 0))
+            
+            screenshake_offset = (random.random() * self.screenshake - self.screenshake / 2, random.random() * self.screenshake - self.screenshake / 2 )
+            self.screen.blit(pygame.transform.scale(self.display, self.screen.get_size()), screenshake_offset)
             pygame.display.update()         #calling update function to update the screen frequently to avoid black screen
             self.clock.tick(60)             #tries to run the loop at fixed fps
 
--- scripts/entities.py
+++ scripts/entities.py
@@ -1,4 +1,4 @@
-import pygame
+import pygame, random
 
 class PhysicsEntity:
     def __init__(self, game, e_type, pos, size):
@@ -71,22 +71,125 @@ class PhysicsEntity:
     def render(self, surf, offset = (0, 0)):
         surf.blit(pygame.transform.flip(self.animation.img(), self.flip, False), (self.pos[0] - offset[0] + self.anim_offset[0], self.pos[1] - offset[1] + self.anim_offset[1]))
 
+class Enemy(PhysicsEntity):
+    def __init__(self, game, pos, size):
+        super().__init__(game, 'enemy', pos, size)
+    
+        self.walking = 0
+        self.attacking = False
+        self.hit = 0
+        self.attack_delay = 0
+    
+    def update(self, tilemap, movement = (0, 0)):
+        self.attacking = False
+        if self.attack_delay:
+            self.attack_delay += 1
+            
+        if self.walking:
+            if tilemap.solid_check((self.rect().centerx + (-7 if self.flip else 7), self.pos[1] + 23)):
+                if (self.collisions['right'] or self.collisions['left']):
+                    self.flip = not self.flip
+                else:    
+                    movement = (movement[0] - 0.5 if self.flip else 0.5, movement[1])
+            else:
+                self.flip = not self.flip
+            self.walking = max(0, self.walking - 1)
+        elif random.random() < 0.01:
+            self.walking = random.randint(30, 120)
+        dis = (self.game.player.pos[0] - self.pos[0], self.game.player.pos[1] - self.pos[1])
+        if abs(dis[1] < 16 and dis[1] > -16):
+            if (self.flip and (dis[0] < 0 and dis[0] > -16)):   #player to the left of enemy and enemy is facing left
+                self.attack_delay += 1
+                if self.attack_delay >= 30:
+                    self.attacking = True
+                self.walking = 0
+            elif (not self.flip and (dis[0] < 0 and dis[0] > -16)):
+                self.flip = not self.flip
+                self.attack_delay += 1
+                if self.attack_delay >= 30:
+                    self.attacking = True
+                self.walking = 0
+            if (not self.flip and (dis[0] > 0 and dis[0] < 16)):    #player to the right of enemy and enemy is facing right
+                self.attack_delay += 1
+                if self.attack_delay >= 30:
+                    self.attacking = True
+                self.walking = 0
+            elif (self.flip and (dis[0] > 0 and dis[0] < 16)):
+                self.flip = not self.flip
+                self.attack_delay += 1
+                if self.attack_delay >= 30:
+                    self.attacking = True
+                self.walking = 0
+        else:
+            self.attack_delay = 0
+
+        super().update(tilemap, movement=movement)
+        
+        if self.attacking:
+            self.set_action('attack')
+        elif movement[0] != 0:
+            self.set_action('run')
+        else:
+            self.set_action('idle')
+            
+        if self.attacking:
+            self.game.player.killed = True
+        
+        if self.game.player.hit_delay >= 20:
+            if self.rect().colliderect(self.game.player.rect()):
+                self.game.screenshake = max(1, self.game.screenshake)
+                return True
+        
+
 class Player(PhysicsEntity):
     def __init__(self, game, pos, size):
         super().__init__(game, 'player', pos, size)
         self.air_time = 0
+        self.jumps = 2
+        self.attacks = 0
+        self.killed = False
+        self.hit_delay = 0
     
     def update(self, tilemap, movement):
         super().update(tilemap, movement=movement)
         
+        if self.air_time > 120:
+            self.game.dead += 1
+            self.game.screenshake = max(16, self.game.screenshake)
+        
+        if self.attacks >= 1:
+            self.hit_delay += 1
+        else:
+            self.hit_delay = 0
+        
         self.air_time += 1
         if self.collisions['down']:
             self.air_time = 0
-            
-        if self.air_time > 4:
+            self.jumps = 2
+        attack_time = 60
+        if self.attacks > 0:
+            self.set_action('attack')
+        elif self.jumps == 0 :
+            self.set_action('roll')    
+        elif self.air_time > 4:
             self.set_action('jump')
         elif movement[0] != 0:
             self.set_action('run')
         else:
             self.set_action('idle')
-        
+
+    def jump(self):
+        if self.jumps == 1:
+            self.velocity[1] = -2
+            self.jumps -= 1
+            self.air_time = 5 #immediately switch to jump animation
+        elif self.jumps == 2:
+            self.velocity[1] = -3.5
+            self.jumps -= 1
+            self.air_time = 5
+
+    def attack(self):
+        if not self.attacks:
+            self.attacks += 1
+        else:
+            self.attacks = 0
